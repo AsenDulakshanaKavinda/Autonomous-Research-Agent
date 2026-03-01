a6a98ec4b7c7d64c7c208920387eb1d91d4097ac
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-03-01 08:36:23 +0530

    added the ArxivSearchTool

---
+++ tools/arxiv_tool.py
@@ -0,0 +1,33 @@
+
+from crewai.tools import BaseTool
+from pydantic import BaseModel, Field
+from typing import Type
+import urllib.request
+import urllib.parse
+
+class ArxivSearchInput(BaseModel):
+    """ Input schema for ArxivSearchTool """
+    query: str = Field(..., description="search topic for arXive papers")
+    max_result: int = Field(default=3, description="Number of results to fetch")
+
+class ArxivSearchTool(BaseTool):
+    name: str = "ArXive Research search tool"
+    description: str = "Search academic papers from arXiv based on a topic"
+    args_schema: Type[BaseModel] = ArxivSearchInput
+
+    def _run(self, query: str, max_results: int = 3) -> str:
+        try:
+            encoded_query = urllib.parse.quote(query)
+
+            url = f"http://export.arxiv.org/api/query?search_query=all:{encoded_query}&start=0&max_results={max_results}"
+
+            with urllib.request.urlopen(url) as response:
+                data = response.read().decode("utf-8")
+
+            return data
+        
+        except Exception as e:
+            return f"Error fetching arXiv data: {str(e)}"
+
+
+arxiv_tool = ArxivSearchTool()
